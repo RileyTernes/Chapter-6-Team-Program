7d75cc3d26e3f4fd2c617081cf0c4298b43b2596
Braxton Hartley <brxtnhartley@gmail.com>
2024-12-04 13:22:23 -0600

    as

---
+++ Chapter 6 Team Project.py
@@ -0,0 +1,31 @@
+#Chapter 6 Team Project
+
+def main():
+    #calls menu to get choice
+    #calls function according to choice
+
+def menu():
+    #displays menu
+    #returns choice
+
+def add():
+    #opens file
+    #takes input for contact information
+    #writes it to contact.txt
+    #outputs a confirmation message
+    #closes file
+
+def search():
+    #Opens file
+    #searches for name that is asked for
+    #prints the values of the associated name
+    #closes the file
+    
+def edit():
+    #opens file
+    #takes
+
+def delete():
+
+    
+def display():
