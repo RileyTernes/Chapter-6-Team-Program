21f2779b879ce5393fccfae655b6aee49757c900
Liam-Dowell <lcdowell10@gmail.com>
2024-12-04 13:14:40 -0600

    Update Chapter 6 Team Project.py

--- Chapter 6 Team Project.py
+++ Chapter 6 Team Project.py
@@ -1,12 +1,44 @@
 #Chapter 6 Team Project
 
 def main():
-    #calls menu to get choice
-    #calls function according to choice
+    #main recieves no arguements
+    #runs all of the programs 
+    #outputs a thank you message
+    choice = int(menu())
+
+    while choice < 1 and choice > 5:
+        print("Invalid response")
+    while choice != 6:
+        if choice == 1:
+            add()
+        if choice == 2:
+            search()
+        if choice == 3:
+            edit()
+        if choice == 4:
+            delete()
+        if choice == 5:
+            display()
+        choice = int(menu())
+    print("Have a good day.")
 
 def menu():
-    #displays menu
+    #menu recieves no arguements
+    #displays all of the options
     #returns choice
+
+    print("Hello")
+    print("1) Add a contact")
+    print("2) Search a contact")
+    print("3) Edit a contact")
+    print("4) Delete a contact")
+    print("5) Display all contacts")
+    print("6) Exit")
+
+    choice = int(input("Select and option: "))
+
+    return choice
+
 
 def add():
     #opens file
