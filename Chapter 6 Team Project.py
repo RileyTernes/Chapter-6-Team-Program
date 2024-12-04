e8dc18b52ae09e1aa3bbcd9267d00ae8b1b1c8ef
RileyTernes <riley.ternes@gmail.com>
2024-12-04 13:47:06 -0600

    Update Chapter 6 Team Project.py

--- Chapter 6 Team Project.py
+++ Chapter 6 Team Project.py
@@ -1,5 +1,5 @@
 #Chapter 6 Team Project
-
+import os
 def main():
     #calls menu to get choice
     #calls function according to choice
@@ -7,15 +7,31 @@ def main():
 def menu():
     #displays menu
     #returns choice
-
 def add():
     #opens file
     #takes input for contact information
     #writes it to contact.txt
     #outputs a confirmation message
     #closes file
-    
-    
+    choice = 'y'
+    infile = open("contact.txt", "a")
+    while choice.lower() == 'y':
+        print("Enter the contact information:\n")
+        #get input
+        name = input("Name: ")
+        street_address = input("Street address: ")
+        phone_number = input("Phone number: ")
+        email_address = input("Email address: ")
+        #write it to the text
+        infile.write(name + '\n')
+        infile.write(street_address + '\n')
+        infile.write(phone_number + '\n')
+        infile.write(email_address + '\n')
+        choice = input("\nDo you wish to enter another contacts information? (y to continue): ")
+    #close file
+    infile.close()
+    #confirm that it's written
+    print("\nAll data appended to coffee.txt.")
 def edit():
     #opens file
     #takes
